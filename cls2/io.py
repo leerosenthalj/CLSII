4bbc1d516aec0225fe9df13422814465161b2976
BJ Fulton <bjfultn@gmail.com>
2020-12-22 10:35:00 -0800

    catching repo up

---
+++ cls2/io.py
@@ -0,0 +1,17 @@
+
+import numpy as np
+import csv
+
+
+def retrieve_name(var):
+    callers_local_vars = inspect.currentframe().f_back.f_locals.items()
+    return [var_name for var_name, var_val in callers_local_vars if var_val is var]
+
+
+def save_table(table, key, outname):
+
+    with open(outname, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(['name'])
+        for val in np.unique(table[key]):
+            writer.writerow([val])
--- cls2/occur.py
+++ cls2/occur.py
@@ -205,7 +205,7 @@ class Hierarchy(object):
                 
         axis  = []
         msini = []
-        self.planetnames = np.unique([x[:-2] + x[-1] for x in pop.columns]) 
+        self.planetnames = np.unique([x[:-2] + x[-1] for x in pop.columns])
         self.starnames   = np.unique([x[:-1] for x in self.planetnames])
         self.nplanets    = len(self.planetnames)
         self.nsamples    = len(self.pop)
@@ -263,7 +263,7 @@ class Hierarchy(object):
         return occur    
 
     def lnlike(self, theta): 
-        if np.any((theta <= 0) + (theta > 4*self.ceiling)):
+        if np.any((theta <= 0) + (theta > 20*self.ceiling)):
             return -np.inf
         sums = []
         for planet in self.planetnames:
@@ -498,4 +498,220 @@ class BrokenPowerLaw(object):
     
     def run(self):
         self.max_like()
-        self.sample()  
+        self.sample()
+
+
+class Insol(object):
+    """Do hierarchical Bayesian sampling of occurrence posteriors, based on DFM et al. 2014.
+    Args:
+        pop (pandas DataFrame): dataframe of planet parameter chains
+
+    """
+    def __init__(self, pop, completeness, res=4, bins=np.array([[[np.log(0.1), np.log(1000)], 
+                                                                 [np.log(2.), np.log(6000)]]]),
+                                                                  chainname='insol_chains.csv'):
+        # TO-DO: Replace single-param planets with paths to posteriors.
+        self.pop          = pop # Replace pairs of m & a with chains
+        self.completeness = completeness # Completeness grid, defined as class object below.
+        self.completeness.completeness_grid([0.05, 2000], [3, 7000])
+        # Fill in completeness nans.
+        self.completeness.grid[2][np.isnan(self.completeness.grid[2])] = 1. #0.99
+        
+        
+        self.res = res # Resolution for logarithmic completeness integration.
+        self.bins = bins # Logarithmic bins in msini/axis space.
+        self.nbins = len(self.bins)
+        self.lna_edges = np.unique(self.bins[:, 0])
+        self.lnm_edges = np.unique(self.bins[:, 1])
+        self.nabins = len(self.lna_edges) - 1
+        self.nmbins = len(self.lnm_edges) - 1
+        
+        # Compute bin centers and widths.
+        self.bin_widths  = np.diff(self.bins)
+        self.bin_centers = np.mean(self.bins, axis=2)
+        self.bin_areas   = self.bin_widths[:,0]*self.bin_widths[:,1]
+        
+        # Pre-compute integrated completeness for each bin.
+        self.Qints = np.zeros(self.nbins)
+        for n, binn in enumerate(self.bins):
+            for i in np.arange(4): #self.res
+                for j in np.arange(4):
+                    lna_av = binn[0][0] + (0.25*i + 0.125)*(binn[0][1] - binn[0][0])
+                    lnm_av = binn[1][0] + (0.25*j + 0.125)*(binn[1][1] - binn[1][0])     
+                    self.Qints[n] += (self.bin_areas[n][0]/self.res**2) * \
+                                      self.completeness.interpolate(np.exp(lna_av), 
+                                                                    np.exp(lnm_av))
+                
+        axis  = []
+        msini = []
+        self.planetnames = np.unique([x[:-2] + x[-1] for x in pop.columns])
+        self.starnames   = np.unique([x[:-1] for x in self.planetnames])
+        self.nplanets    = len(self.planetnames)
+        self.nsamples    = len(self.pop)
+        self.nstars      = len(self.starnames)
+        
+        medians = pop.median() # Along chain axis, once using chains.
+        for name in self.planetnames:
+            axis.append(medians[[name[:-1] + 'S' + name[-1]]][0])
+            msini.append(medians[[name[:-1] + 'M' + name[-1]]][0])            
+        self.pop_med = pd.DataFrame.from_dict({'insol':axis, 'msini':msini})
+        
+        self.chainname = chainname
+        
+    def max_like(self):
+        ### Approximate max-likelihood occurrence values, with which to seed MCMC.
+        mlvalues = np.empty((0, 2))
+        for n, binn in enumerate(self.bins):
+            # Integrate completeness across each individual bin.
+            a1 = np.exp(binn[0][0])
+            a2 = np.exp(binn[0][1])
+            M1 = np.exp(binn[1][0])
+            M2 = np.exp(binn[1][1])
+            planets = self.pop_med.query('insol >= @a1 and insol < @a2 and \
+                                         msini >= @M1 and msini < @M2')
+            nplanets = len(planets)
+            ml  = nplanets/self.Qints[n]
+            uml = ml/np.sqrt(nplanets)
+            if not np.isfinite(ml):
+                ml = 0.01 
+            if not np.isfinite(uml):
+                uml = 1.
+            mlvalues = np.append(mlvalues, np.array([[ml, uml]]), axis=0) 
+        mlvalues[np.isnan(mlvalues)] = 0.01
+        mlvalues[mlvalues == 0] = 0.01
+        self.mlvalues = mlvalues
+        self.ceiling = np.amax(mlvalues)
+        
+    def occurrence(self, lna, lnm, theta):
+        # Select appropriate bins, given lna & lnm.
+        ia = np.atleast_1d(np.digitize(lna, self.lna_edges) - 1)
+        im = np.atleast_1d(np.digitize(lnm, self.lnm_edges) - 1)
+        iao = np.copy(ia)
+        imo = np.copy(im)
+        ia[ia < 0] = 0
+        im[im < 0] = 0
+        ia[ia > self.nabins - 1] = self.nabins - 1
+        im[im > self.nmbins - 1] = self.nmbins - 1
+        
+        occur = theta[ia + im*self.nabins]
+        # Return filler value for samples outside of the bin limits.
+        occur[iao < 0] = 0.01 
+        occur[imo < 0] = 0.01 
+        occur[iao > self.nabins - 1] = 0.01 
+        occur[imo > self.nmbins - 1] = 0.01 
+        return occur    
+
+    def lnlike(self, theta): 
+        if np.any((theta <= 0) + (theta > 20*self.ceiling)):
+            return -np.inf
+        sums = []
+        for planet in self.planetnames:
+            probs = []
+            sample_a = np.array(self.pop[planet[:-2] + '_S' + planet[-1]])
+            sample_M = np.array(self.pop[planet[:-2] + '_M' + planet[-1]])
+            probs = self.completeness.interpolate(sample_a, sample_M)*self.occurrence(
+                                           np.log(sample_a), np.log(sample_M), theta)
+            sums.append(np.sum(probs))
+        
+        # Integrate the observed occurrence over all bins. 
+        nexpect = 0
+        for i, binn in enumerate(self.bins):
+            for j in np.arange(4): 
+                for k in np.arange(4):
+                    lna_av = binn[0][0] + (0.25*j + 0.125)*(binn[0][1] - binn[0][0])
+                    lnm_av = binn[1][0] + (0.25*k + 0.125)*(binn[1][1] - binn[1][0])                    
+                    nexpect += (self.bin_areas[i][0]/16)*self.completeness.interpolate(
+                                                                        np.exp(lna_av), 
+                                                                        np.exp(lnm_av))*self.occurrence(
+                                                                        lna_av, lnm_av, theta)
+
+        ll = -nexpect + np.sum(np.log(np.array(sums)/self.nsamples))
+        if not np.isfinite(ll):
+            return -np.inf
+
+        return ll
+    
+    def gpprior(self, theta, mu, l0, la, lm):
+        ### Prior on occurrence. Gaussian process, for smoothly changing bin heights.
+        if not 0 < mu < 4*self.ceiling:
+            return -np.inf
+        if not -2 < l0 < 6:
+            return -np.inf
+        if not -2 < la < 1:
+            return -np.inf
+        if not -2 < lm < 0:
+            return -np.inf
+        if np.any((theta <= 0) + (theta > 4*self.ceiling)): 
+            return -np.inf
+
+        # Compute Euclidean distance between bins, [∆i − ∆j]T Σ−1[∆i − ∆j]
+        mini_inv_covar = np.array([[np.exp(la)**-1, 0], [0, np.exp(lm)**-1]])
+        X              = np.matmul(self.bin_centers, mini_inv_covar)
+        distance       = scipy.spatial.distance.cdist(X, X, 'sqeuclidean')
+        
+        K        = np.exp(l0)*np.exp(-0.5*distance)
+        s, logdK = np.linalg.slogdet(K)
+                
+        y  = theta - mu
+        F  = cho_factor(K)
+        lp = -0.5*(logdK + np.dot(y, cho_solve(F, y)))
+        
+        if not np.isfinite(lp):
+            return -np.inf
+        return lp
+    
+    def lnpost(self, theta):
+        return self.lnlike(theta) 
+    
+    def gppost(self, theta_gp):
+        return self.lnlike(theta_gp[:-4]) + self.gpprior(theta_gp[:-4], 
+                                                         theta_gp[-4], 
+                                                         theta_gp[-3], 
+                                                         theta_gp[-2], 
+                                                         theta_gp[-1])
+    
+    def sample(self, gp=False, parallel=False, save=True):
+        nwalkers = 4*self.nbins
+        ndim = self.nbins 
+        pos = np.array([np.abs(self.mlvalues[:, 0] + 0.01*np.random.randn(ndim)) \
+                                                 for i in np.arange(nwalkers)]) + 0.01
+        if gp:
+            ndim += 4
+            mu0 = np.mean(self.mlvalues[:, 0])
+            l00 = 0
+            la0 = 0
+            lm0 = 0
+            pos = np.append(pos, np.array([mu0, l00, la0, lm0]) + \
+                                 np.array([0.05*np.random.randn(4) \
+                                 for i in np.arange(nwalkers)]), axis=1)
+
+        if parallel:
+            with Pool(8) as pool:
+                if gp:
+                    self.sampler = emcee.EnsembleSampler(nwalkers, ndim, self.gppost, pool=pool)
+                else:
+                    self.sampler = emcee.EnsembleSampler(nwalkers, ndim, self.lnpost, pool=pool)
+                self.sampler.run_mcmc(pos, 1000, progress=True)
+                self.chains = self.sampler.chain[:, 100:, :].reshape((-1, ndim))
+        else:
+            if gp:
+                self.sampler = emcee.EnsembleSampler(nwalkers, ndim, self.gppost)
+            else:
+                self.sampler = emcee.EnsembleSampler(nwalkers, ndim, self.lnpost)
+            self.sampler.run_mcmc(pos, 300, progress=True)           
+            self.chains = self.sampler.chain[:, 100:, :].reshape((-1, ndim))    
+            
+        if save:
+            chaindb = pd.DataFrame() 
+            for n, binn in enumerate(self.bins):
+                chaindb['gamma{}'.format(n)] = self.chains[:, n]
+            if gp:
+                chaindb['lmu)'] = self.chains[:, -4]
+                chaindb['ll0)'] = self.chains[:, -3]
+                chaindb['lla)'] = self.chains[:, -2]
+                chaindb['llm)'] = self.chains[:, -1]
+            chaindb.to_csv(self.chainname)           
+    
+    def run(self):
+        self.max_like()
+        self.sample()
